4e4ba93a2196bfc5fc8cabf4e7260de568cd7b12
m01nak <m01nak77@gmail.com>
2023-06-19 19:59:20 +0530

    changed logging framework to log4j2 from logback

--- src/main/java/com/moinak/springbootplayground/SpringBootPlaygroundApplication.java
+++ src/main/java/com/moinak/springbootplayground/SpringBootPlaygroundApplication.java
@@ -1,13 +1,17 @@
 package com.moinak.springbootplayground;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootPlaygroundApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootPlaygroundApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPlaygroundApplication.class, args);
+		logger.info("Hello");
 	}
 
 }
